8b2bd9488514545f7c4804b95a8d5c8ef4a144df
armenh <avarmenhg@gmail.com>
2017-03-31 09:23:13 -0400

    ngrx store freeze added

--- src/app/store/reducers/rootReducer.ts
+++ src/app/store/reducers/rootReducer.ts
@@ -2,8 +2,13 @@ import {combineReducers} from '@ngrx/store';
 import {uiState} from './uiStateReducer';
 import {storeData} from './uiStoreDataReducer';
 import {INITIAL_APPLICATION_STATE} from '../application-state';
+import {compose} from '@ngrx/core/compose';
+import {storeFreeze} from 'ngrx-store-freeze';
 
-const reducers = combineReducers({
+
+const metaReducers = [storeFreeze, combineReducers];
+
+const reducers = compose(...metaReducers)({
   uiState,
   storeData
 });
